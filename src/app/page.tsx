9954e4d73561f7a0595b576e72dbde922d9e15a2
danielmahler1 <danielmahler34@gmail.com>
2024-12-24 22:06:41 -0500

    further fixing unescaped entities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
+/* eslint-disable react/no-unescaped-entities */
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <p>Welcome to Daniel Mahler's Website. Coming Soon.</p>
+        <p>Welcome to Daniel Mahler's Portfolio. Coming Soon.</p>
         </ol>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
