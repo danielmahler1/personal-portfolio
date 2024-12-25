3e692ee23bb817f95433ef9ec47a0bb2b02b19df
danielmahler1 <danielmahler34@gmail.com>
2024-12-24 22:00:16 -0500

    fixed apostrophe escape

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          Welcome to Daniel Mahler's Website. Coming Soon.
+          <p>Welcome to Daniel Mahler's Website. Coming Soon.</p>
         </ol>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
